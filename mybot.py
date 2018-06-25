76345facc49473ddbaf6711465dad1315889a458
Paal Braathen <paal.braathen@usit.uio.no>
2018-06-25 13:54:43 +0200

    Linting, flake8

--- mybot.py
+++ mybot.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
-#coding: utf-8
+# coding: utf-8
 from datetime import datetime
 from optparse import OptionParser
-from pprint import pprint
 from threading import Timer
 import dateparser
 import sys
@@ -23,6 +22,7 @@ if sys.version_info < (3, 0):
 else:
     raw_input = input
 
+
 class MyBot(sleekxmpp.ClientXMPP):
 
     def __init__(self, jid, password, room, nick):
@@ -30,7 +30,7 @@ class MyBot(sleekxmpp.ClientXMPP):
 
         self.room = room
         self.nick = nick
-        
+
         self.add_event_handler("session_start", self.start)
         self.add_event_handler("groupchat_message", self.muc_message)
         self.add_event_handler("message", self.message)
@@ -55,7 +55,7 @@ class MyBot(sleekxmpp.ClientXMPP):
 
             if msg['body'].startswith("!reminder "):
                 now = datetime.now()
-                max_seconds = 3600*24*7 # Do not create reminders > 1 week
+                max_seconds = 3600 * 24 * 7  # Do not create reminders > 1 week
                 try:
                     date_string, message = [s.strip() for s in msg['body'][10:].split(";", 1)]
                 except:
@@ -86,6 +86,7 @@ class MyBot(sleekxmpp.ClientXMPP):
         if msg['type'] in ('chat', 'normal'):
             msg.reply("I'm a bot. Hurr durr.").send()
 
+
 if __name__ == '__main__':
     optp = OptionParser()
 
@@ -105,13 +106,13 @@ if __name__ == '__main__':
         opts.room = raw_input("MUC room: ")
     if opts.nick is None:
         opts.nick = raw_input("MUC nickname: ")
-    
+
     password = getpass.getpass("Password: ")
 
     xmpp = MyBot(opts.jid, password, opts.room, opts.nick)
-    xmpp.register_plugin('xep_0030') # Service Discovery
-    xmpp.register_plugin('xep_0045') # Multi-User Chat
-    xmpp.register_plugin('xep_0199') # XMPP Ping
+    xmpp.register_plugin('xep_0030')  # Service Discovery
+    xmpp.register_plugin('xep_0045')  # Multi-User Chat
+    xmpp.register_plugin('xep_0199')  # XMPP Ping
 
     # Connect to the XMPP server and start processing XMPP stanzas.
     if xmpp.connect():
@@ -119,4 +120,3 @@ if __name__ == '__main__':
         print("Done")
     else:
         print("Unable to connect.")
-
